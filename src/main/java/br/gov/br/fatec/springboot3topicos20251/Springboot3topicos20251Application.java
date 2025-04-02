79c6118e175ca4f26a1ee1f4bb8eea800faa001f
Juliano <julianoprado98@gmail.com>
2025-04-02 02:56:01 -0300

    first commit

---
+++ src/main/java/br/gov/br/fatec/springboot3topicos20251/Springboot3topicos20251Application.java
@@ -0,0 +1,13 @@
+package br.gov.br.fatec.springboot3topicos20251;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3topicos20251Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3topicos20251Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/br/fatec/springboot3topicos20251/Springboot3topicos20251ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.br.fatec.springboot3topicos20251;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3topicos20251ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
